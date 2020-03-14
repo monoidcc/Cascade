d3ff61e50c96fcff8f1cc1a73eced088b4956e4d
Yoshiya Hinosawa <stibium121@gmail.com>
2020-03-14 15:43:48 +0900

    feat: introduce capsid

--- src/script.ts
+++ src/script.ts
@@ -3,115 +3,130 @@ import bezier = require('bezier-easing')
 import Color = require('color')
 import { Rect, Motion, Wave, WaveRect, Result } from './models'
 import { dice } from './random'
-import { qs, byId, Ctx } from './dom'
+import { Ctx } from './dom'
+import { component, on, emits, notifies } from 'capsid'
 
-let width = 0
-let height = 0
-let ctx: Ctx
-const wave: Wave = new Wave()
-const result: Result = new Result()
-const loop = gameloop(main, 60)
-
-const initCanvas = () => {
-  const canvas = byId<HTMLCanvasElement>('main-canvas')
-  const main = byId<HTMLElement>('main')
-  ctx = canvas.getContext('2d')!
-  width = main.clientWidth
-  height = main.clientHeight
-  canvas.width = width
-  canvas.height = height
-}
-
-function initButtons() {
-  qs('.a-btn').addEventListener('click', () => {
-    newWave()
-  })
-  qs('.b-btn').addEventListener('click', () => {
-    result.clear()
-    wave.eject()
-  })
-}
-
-function main() {
-  const finished = wave.step()
-  if (finished) {
-    result.add(...finished.map(wr => wr.rect))
+@component('main-canvas')
+export class MainCanvas {
+  width = 0
+  height = 0
+  ctx: Ctx
+  wave: Wave
+  result: Result
+  loop: any
+  el: HTMLCanvasElement
+  easing0 = bezier(0.42, 0, 0.58, 1)
+  colors = [
+    Color().hue(dice(360)).saturationl(dice(100)).lightness(dice(100)).alpha(0.35),
+    Color().hue(dice(360)).saturationl(dice(100)).lightness(dice(100)).alpha(0.35),
+    Color().hue(dice(360)).saturationl(dice(100)).lightness(dice(100)).alpha(0.35),
+    Color().hue(dice(360)).saturationl(dice(100)).lightness(dice(100)).alpha(0.35)
+  ]
+  constructor() {
+    this.wave = new Wave()
+    this.result = new Result()
+    this.loop = gameloop(this.main, 60)
+    this.el = document.createElement('a')
   }
-  ctx.clearRect(0, 0, width, height)
-  ctx.fillStyle = '#000'
-  drawResult(ctx, result)
-  drawWave(ctx, wave)
+
+  __mount__() {
+    const main = this.el.parentElement
+    this.ctx = this.el.getContext('2d')!
+    const canvasSize = Math.min(main.clientWidth, main.clientHeight)
+    this.el.width = this.width = canvasSize
+    this.el.height = this.height = canvasSize
+    this.loop.run()
+
+  }
+
+  main = () => {
+    const finished = this.wave.step()
+    if (finished) {
+      this.result.add(...finished.map(wr => wr.rect))
+    }
+    this.ctx.clearRect(0, 0, this.width, this.height)
+    this.drawRects(this.result.rects)
+    this.drawRects(this.wave.toArray())
+
+  }
+
+  rotateColor(): void {
+    this.colors.push(this.colors.shift()!)
+  }
+
+  newWave(): void {
+    this.rotateColor()
+    const min = Math.min(this.width, this.height)
+    const w = dice(min / 3) + min / 6
+    const hw = w / 2
+    const ratio = Math.random()
+    const dw = this.width * ratio
+    const dh = this.height * ratio
+    const alpha = dice(1)
+    const c0 = this.colors[0].alpha(alpha).toString()
+    const c1 = this.colors[1].alpha(alpha).toString()
+    const c2 = this.colors[2].alpha(alpha).toString()
+    const c3 = this.colors[3].alpha(alpha).toString()
+    const r0 = new Rect(0 - hw, dh, w, w, c0)
+    const r1 = new Rect(this.width - dw, -hw, w, w, c1)
+    const r2 = new Rect(this.width + hw, this.height - dh, w, w, c2)
+    const r3 = new Rect(dw, this.height + hw, w, w, c3)
+    const frames = 60
+    const m0 = new Motion(frames, this.easing0, w, 0)
+    const m1 = new Motion(frames, this.easing0, 0, w)
+    const m2 = new Motion(frames, this.easing0, -w, 0)
+    const m3 = new Motion(frames, this.easing0, 0, -w)
+    m0.init(r0.x, r0.y)
+    m1.init(r1.x, r1.y)
+    m2.init(r2.x, r2.y)
+    m3.init(r3.x, r3.y)
+    this.wave.add(new WaveRect(r0, m0))
+    this.wave.add(new WaveRect(r1, m1))
+    this.wave.add(new WaveRect(r2, m2))
+    this.wave.add(new WaveRect(r3, m3))
+  }
+
+  drawRects(rects: Rect[]) {
+    rects.forEach(rect => {
+      this.ctx.fillStyle = rect.color
+      this.ctx.fillRect(rect.left(), rect.top(), rect.width, rect.height)
+    })
+  }
+
+  @on('a')
+  a() {
+    this.newWave()
+  }
+
+  @on('b')
+  b() {
+    this.wave.eject()
+    this.result.clear()
+  }
 }
 
-const easing0 = bezier(0.42, 0, 0.58, 1)
-const colors = [
-  Color().hue(dice(360)).saturationl(dice(100)).lightness(dice(100)).alpha(0.35),
-  Color().hue(dice(360)).saturationl(dice(100)).lightness(dice(100)).alpha(0.35),
-  Color().hue(dice(360)).saturationl(dice(100)).lightness(dice(100)).alpha(0.35),
-  Color().hue(dice(360)).saturationl(dice(100)).lightness(dice(100)).alpha(0.35)
-]
+@component('controls')
+export class Controls {
+  @on.click.at('.a-btn')
+  @emits('a')
+  a() {
+  }
 
-function rotateColor(): void {
-  colors.push(colors.shift()!)
+  @on.click.at('.b-btn')
+  @emits('b')
+  b() {
+  }
 }
 
-function gen(): void {
-  rotateColor()
-  const min = Math.min(width, height)
-  const w = dice(min / 3) + min / 6
-  const hw = w / 2
-  const ratio = Math.random()
-  const dw = width * ratio
-  const dh = height * ratio
-  const alpha = dice(1)
-  const c0 = colors[0].alpha(alpha).toString()
-  const c1 = colors[1].alpha(alpha).toString()
-  const c2 = colors[2].alpha(alpha).toString()
-  const c3 = colors[3].alpha(alpha).toString()
-  const r0 = new Rect(0 - hw, dh, w, w, c0)
-  const r1 = new Rect(width - dw, -hw, w, w, c1)
-  const r2 = new Rect(width + hw, height - dh, w, w, c2)
-  const r3 = new Rect(dw, height + hw, w, w, c3)
-  const frames = 60
-  const m0 = new Motion(frames, easing0, w, 0)
-  const m1 = new Motion(frames, easing0, 0, w)
-  const m2 = new Motion(frames, easing0, -w, 0)
-  const m3 = new Motion(frames, easing0, 0, -w)
-  m0.init(r0.x, r0.y)
-  m1.init(r1.x, r1.y)
-  m2.init(r2.x, r2.y)
-  m3.init(r3.x, r3.y)
-  wave.add(new WaveRect(r0, m0))
-  wave.add(new WaveRect(r1, m1))
-  wave.add(new WaveRect(r2, m2))
-  wave.add(new WaveRect(r3, m3))
-}
+@component('body')
+export class Body {
+  @on('a')
+  @notifies('a', '.main-canvas')
+  a() {
+  }
 
-function newWave() {
-  gen()
-}
-
-function init() {
-  loop.run()
-}
-
-function drawResult(ctx: Ctx, result: Result) {
-  drawRects(ctx, result.rects)
-}
-
-function drawWave(ctx: Ctx, wave: Wave) {
-  drawRects(ctx, wave.toArray())
-}
-
-function drawRects(ctx: Ctx, rects: Rect[]) {
-  rects.forEach(rect => {
-    ctx.fillStyle = rect.color
-    ctx.fillRect(rect.left(), rect.top(), rect.width, rect.height)
-  })
-}
-
-;(window as any).onload = async () => {
-  initCanvas()
-  initButtons()
-  init()
+  @on('b')
+  @notifies('b', '.main-canvas')
+  b() {
+  }
 }
