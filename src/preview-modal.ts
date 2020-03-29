dcc4db101ed997cab5afbeab7cee76497c8363f7
Yoshiya Hinosawa <stibium121@gmail.com>
2020-03-30 02:29:50 +0900

    feat: add save and cancel buttons

--- src/preview-modal.ts
+++ src/preview-modal.ts
@@ -9,7 +9,8 @@ import { Work } from './models'
 @component('preview-modal')
 @sub('preview-modal')
 @innerHTML(`
-  <canvas class="preview-canvas" />
+  <canvas class="preview-canvas"></canvas>
+  <div class="preview-controls"></div>
 `)
 @is(css`
   display: none;
@@ -71,5 +72,45 @@ class PreviewCanvas {
   @on.click
   onClick(e: Event) {
     e.stopPropagation()
+  }
+}
+
+@is(css`
+  background-color: white;
+  margin: 15px 0;
+  width: 80%;
+  height: 12%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  button {
+    width: 150px;
+    height: 36px;
+    border-width: 0;
+    background-color: hsla(220,20%,80%,0.8);
+  }
+`)
+@innerHTML(`
+  <button class="save">SAVE</button>
+  <button class="cancel">CANCEL</button>
+`)
+@component('preview-controls')
+class PreviewControls {
+  @on.click
+  onClick(e: Event) {
+    e.stopPropagation()
+  }
+
+  @on.click.at('.save')
+  @pub('preview-save')
+  previewSave() {
+    alert('save')
+  }
+
+  @on.click.at('.cancel')
+  @pub('preview-cancel')
+  previewCancel() {
+    alert('cancel')
   }
 }
