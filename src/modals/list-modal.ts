c3e21f96c03578ce4feb9baf1341c5f8b24ef89d
Yoshiya Hinosawa <stibium121@gmail.com>
2020-04-03 03:16:02 +0900

    feat: list-item component

--- src/modals/list-modal.ts
+++ src/modals/list-modal.ts
@@ -1,4 +1,4 @@
-import { wired, is, component, on, sub, innerHTML } from 'capsid'
+import { wired, is, component, on, sub, innerHTML, make, get } from 'capsid'
 import { css } from 'emotion'
 import { Artwork, ArtworkRepository } from '../domain/models'
 import { drawArtwork } from '../adapters/canvas'
@@ -17,11 +17,7 @@ import { drawArtwork } from '../adapters/canvas'
   &.show {
     display: flex;
     flex-wrap: wrap;
-  }
-
-  canvas {
-    width: min(25vw, 25vh);
-    height: min(25vw, 25vh);
+    justify-content: flex-start;
   }
 
   .list-controls {
@@ -59,19 +55,61 @@ export class ListModal {
     this.listArea!.innerHTML = ''
     const artworks = await (new ArtworkRepository().get())
 
-    artworks.artworks.forEach((artwork: Artwork) => {
-      const item = document.createElement('canvas')
-      item.width = 150
-      item.height = 150
-      this.listArea!.appendChild(item)
-      drawArtwork(item.getContext('2d')!, artwork, 150, 150)
+    artworks.artworks.map((artwork: Artwork): [Artwork, HTMLDivElement] =>
+      [artwork, this.el!.querySelector(`[dataset-key="${artwork.id}"]`) || document.createElement('div')]
+    ).forEach(([artwork, div]: [Artwork, HTMLDivElement]) => {
+      make<ListItem>('list-item', div).update(artwork)
+      this.listArea!.appendChild(div)
     })
 
     this.el!.classList.add('show')
   }
 
+  @on.click
   @on.click.at('.close-button')
   close() {
     this.el!.classList.remove('show')
+  }
+}
+
+@component('list-item')
+@is(css`
+  display: inline-box;
+  position: relative;
+  width: min(25vw, 25vh);
+  height: min(25vw, 25vh);
+
+  canvas {
+    width: 100%;
+    height: 100%;
+  }
+`)
+@innerHTML(`<canvas></canvas>`)
+export class ListItem {
+  el?: HTMLDivElement
+
+  artwork?: Artwork
+
+  @wired('canvas')
+  canvas?: HTMLCanvasElement
+
+  __mount__(): void {
+    this.canvas!.width = 150
+    this.canvas!.height = 150
+  }
+
+  update(artwork: Artwork): void {
+    this.el!.dataset.key = artwork.id
+    this.artwork = artwork
+    this.draw()
+  }
+
+  draw(): void {
+    drawArtwork(this.canvas!.getContext('2d')!, this.artwork!, this.canvas!.width, this.canvas!.height)
+  }
+
+  @on.click
+  onClick(e: Event): void {
+    e.stopPropagation()
   }
 }
